6b8df24235b8b73dff6538a94a708c4af7e439fe
Jakob-Em <26389546+Jakob-em@users.noreply.github.com>
2023-08-17 20:14:51 +0200

    Upgrade storybook to 7.3.1

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-    framework: '@storybook/angular',
-    core: {
-        builder: 'webpack5',
-    },
-    stories: [
-        "../stories/**/*.stories.mdx",
-        "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
-    addons: [
-        "../preset.js",
-        "@storybook/addon-docs",
-        "@storybook/addon-controls",
-        "@storybook/addon-actions",
-    ],
+  framework: {
+    name: "@storybook/angular",
+    options: {}
+  },
+  core: {},
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["../preset.js", "@storybook/addon-docs", "@storybook/addon-controls", "@storybook/addon-actions", "@storybook/addon-mdx-gfm"]
 };
-
-export default config
+export default config;
