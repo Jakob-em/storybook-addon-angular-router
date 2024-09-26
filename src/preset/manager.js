edfb6eedb3ad8edae5fac4b061617f08a56af4e0
gnikolaos <nikolaos.georgiou@nextlytics.com>
2024-09-26 09:36:30 +0300

    fix preset/manager.js manager-entries failed

--- src/preset/manager.js
+++ src/preset/manager.js
@@ -1,8 +1,21 @@
-import {addons} from "@storybook/preview-api";
+import { ADDON_ID } from "../constants";
 
-import {ADDON_ID} from "../constants";
+const getAddons = async () => {
+  const _previewApi = await import("@storybook/preview-api");
+  if (_previewApi.addons.register) {
+    return _previewApi.addons;
+  }
+
+  const _managerApi = await import("@storybook/manager-api");
+  return _managerApi.addons;
+};
 
 // Register the addon
-addons.register(ADDON_ID, () => {
-
-});
+(async () => {
+  try {
+    const addons = await getAddons();
+    addons.register(ADDON_ID, () => {});
+  } catch (error) {
+    throw new Error("Failed to register the addon");
+  }
+})();
